107371a47b3da10172f1fdf6425c00e8a3a03ebe
Bronty <at3177198@gmail.com>
2025-09-01 07:00:26 +0900

    Change: Add table header

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -10,10 +10,18 @@
         </div>
         
         <table class="table-auto w-[80%] mx-auto mt-8 border-collapse border-slate-400">
+            <thead>
+                <tr class=" border-b border-slate-300">
+                    <th class="text-left py-4">タイトル</th>
+                    <th class="text-left">最終更新日</th>
+                    <th class="text-left">バージョン</th>
+                    <th class="text-left">ダウンロード</th>
+                </tr>
+            </thead>
             <tbody>
                 <tr class=" border-b border-slate-300 my-2" v-for="(material, index) in materials" :key="index">
                     <td class="py-4">{{ material.title }}</td>
-                    <td>更新:{{ material.updated }}</td>
+                    <td>{{ material.updated }}</td>
                     <td>{{ material.version }}</td>
                     <td class="py-2"><UButton :to="material.downloadLink" icon="ic:baseline-download" color="neutral" size="sm" >ダウンロード</UButton></td>
                 </tr>
