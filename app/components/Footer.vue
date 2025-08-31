21df67b815ddf4d126d57288793d4cbee5008c4c
Bronty <at3177198@gmail.com>
2025-09-01 06:55:51 +0900

    Add: License text

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,7 +1,15 @@
 <template>
     <footer class="bg-slate-100 py-8 w-full">
-        <div class="w-[80%] mx-auto">
-            <p class="text-sm text-slate-500">©2025 bronty.net</p>
+        <div class="w-[80%] mx-auto grid place-items-center grid-cols-1 md:grid-cols-2 gap-4">
+            <p class="text-sm text-slate-500 text-left w-full">©2025 bronty.net</p>
+            <div>
+                <h3>このサイトについて</h3>
+                <p class="text-sm text-slate-500">
+                    本サイトでは主にUnityを用いたゲーム制作に関する教材を提供しています。<br>
+                    本サイトで提供している教材は、教育目的でのみ使用、複製、配布することができます。<br>
+                    教材の利用に起因するいかなる損害についても、著者は一切の責任を負いません。
+                </p>
+            </div>
         </div>
     </footer>
 </template>
